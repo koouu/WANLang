f7cc4ea97319141d0a9c6c39952ce523a8e9a763
koouu <nkouta0420@gmail.com>
2021-12-21 00:15:53 +0900

    一つの数字を出力

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+
+enum TokenKind{
+	TK_RESERVED,	// 記号
+  TK_NUM, 			// 整数トークン
+  TK_EOF, 			// 入力の終わりを表すトークン
+};
+
+
+
 int main(int argc,char **argv){
 	
   if (argc != 2) {
@@ -7,9 +16,13 @@ int main(int argc,char **argv){
     return 1;
   }
 
-  printf("#include <iostream>");
+  printf("#include <iostream>\n");
+	printf("using namespace std;\n");
   printf("int main(){\n");
-  printf("	printf(\"hellowould\");\n");
+  printf("	cout<<");
+	printf("%s",argv[1]);
+
+	printf("<<endl;\n");
   printf("	return 0;\n");
   printf("}\n");
   return 0;
