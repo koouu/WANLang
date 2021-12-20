436a1e59958ba4504e3b0b6a140c45524abd76c1
koouu <nkouta0420@gmail.com>
2021-12-20 23:52:06 +0900

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-int main(){
-	printf("hello");
-	return 0;
+int main(int argc,char **argv){
+	
+  if (argc != 2) {
+    printf("因数が足りません\n");
+    return 1;
+  }
+
+  printf("#include <iostream>");
+  printf("int main(){\n");
+  printf("	printf(\"hellowould\");\n");
+  printf("	return 0;\n");
+  printf("}\n");
+  return 0;
 }
